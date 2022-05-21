0d0eabc99f20d84db9b70192e0c5d13f178b3758
ulngollm <ulngollm@gmail.com>
2022-05-21 20:29:51 +0300

    исключены из ExternalStorage методы, общие для всех типов хранилищ

--- app/Http/Services/ExternalStorage.php
+++ app/Http/Services/ExternalStorage.php
@@ -6,19 +6,10 @@ use App\Models\Storage;
 
 interface ExternalStorage
 {
-    public function addStorage(Storage $storage);
-
-    public function renameStorage(int $id);
-
-    public function deleteStorage(int $id);
-
-    public function getStoragesList();
-
     public function filterByType(string $type);
 
     public function getFolderFiles(string $path);
 
     public function getFile(string $path);
-
 
 }
