10bd79ef65ad9950f4dfb886da3fc5b4ed2fd677
ulngollm <ulngollm@gmail.com>
2022-05-21 21:03:09 +0300

    add migrations

---
+++ database/migrations/2022_05_21_140100_create_storage_types_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('storage_types', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->string('driver');//список доступных драйверов см в кастомном конфиге
+            $table->boolean('active')->default(true);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('storage_types');
+    }
+};
