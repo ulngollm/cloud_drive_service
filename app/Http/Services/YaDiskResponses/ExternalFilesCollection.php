8881f84f7f063ce98ff9d4d0fe7c7fd67382549b
ulngollm <ulngollm@gmail.com>
2022-05-22 16:32:56 +0300

    Созданы фабрики для отдельных ответов api

--- app/Http/Services/YaDiskResponses/ExternalFilesCollection.php
+++ app/Http/Services/YaDiskResponses/ExternalFilesCollection.php
@@ -2,11 +2,9 @@
 
 namespace App\Http\Services\YaDiskResponses;
 
-use App\Http\Services\DTO\ExternalFile;
-use Illuminate\Support\Arr;
 use Illuminate\Support\Collection;
 
-class ExternalFilesCollection
+abstract class ExternalFilesCollection
 {
     public function __construct(
         public Collection $items
@@ -19,18 +17,6 @@ class ExternalFilesCollection
         return $this->items;
     }
 
-    public static function fromYaDiskApi(array $json): self
-    {
-        $items = collect(Arr::get($json, '_embedded.items'))
-            ->map(function ($item) {
-                return new ExternalFile(
-                    id: $item['resource_id'],
-                    type: $item['type'],
-                    name: $item['name'],
-                    path: $item['path'] ?? null,
-                    mimeType: $item['mime_type'] ?? null
-                );
-            });
-        return new self($items);
-    }
+    abstract public static function from(array $json): static;
+
 }
---
+++ app/Http/Services/YaDiskResponses/FolderFilesCollection.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Services\YaDiskResponses;
+
+use App\Http\Services\DTO\ExternalFile;
+use Illuminate\Support\Arr;
+
+class FolderFilesCollection extends ExternalFilesCollection
+{
+    public static function from(array $json): static
+    {
+        $items = collect(Arr::get($json, '_embedded.items'))
+            ->map(function ($item) {
+                return new ExternalFile(
+                    id: $item['resource_id'],
+                    type: $item['type'],
+                    name: $item['name'],
+                    path: $item['path'] ?? null,
+                    mimeType: $item['mime_type'] ?? null
+                );
+            });
+        return new static($items);
+    }
+}
