3dd91545e89440a7157d0c00c2fd1382725f6480
ulngollm <ulngollm@gmail.com>
2022-05-24 11:56:27 +0300

    fix тип отношений у StorageType

--- app/Models/StorageType.php
+++ app/Models/StorageType.php
@@ -4,7 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Eloquent\Relations\HasOne;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class StorageType extends Model
 {
@@ -12,8 +12,8 @@ class StorageType extends Model
 
     use HasFactory;
 
-    public function storage(): HasOne
+    public function storage(): HasMany
     {
-        return $this->hasOne(Storage::class);
+        return $this->hasMany(Storage::class);
     }
 }
