f6efd4ec4a279724e16c000b9ffb58633327761a
ulngollm <ulngollm@gmail.com>
2022-05-24 12:57:48 +0300

    добавлен обработчик http ошибок

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -3,6 +3,7 @@
 namespace App\Exceptions;
 
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Symfony\Component\HttpKernel\Exception\HttpException;
 use Throwable;
 
 class Handler extends ExceptionHandler
@@ -46,5 +47,12 @@ class Handler extends ExceptionHandler
         $this->reportable(function (Throwable $e) {
             //
         });
+
+        $this->renderable(function (HttpException $e) {
+            return response()
+                ->json(['error' => $e->getMessage()])
+                ->setStatusCode($e->getStatusCode());
+        });
+
     }
 }
