3ec56aeaf3d81981730539f3d272881f1bf0751c
ulngollm <ulngollm@gmail.com>
2022-05-25 18:42:44 +0300

    add views

---
+++ resources/views/layout/filter.blade.php
@@ -0,0 +1,17 @@
+<ul class="nav nav-pills card-header-pills">
+    <li class="nav-item">
+        <a class="nav-link disabled" href="#"> Все</a>
+    </li>
+    <li class="nav-item">
+        <a class="nav-link disabled"> Картинки</a>
+    </li>
+    <li class="nav-item">
+        <a class="nav-link disabled"> Текст</a>
+    </li>
+    <li class="nav-item">
+        <a class="nav-link disabled" href="#"> Видео</a>
+    </li>
+    <li class="nav-item">
+        <a class="nav-link disabled"> Аудио</a>
+    </li>
+</ul>
